5f13ea7fe01580aed688eedd76fb0df55f664d2b
Re44e <re44e@outlook.com.br>
2020-06-26 23:48:46 -0300

    "Regra do escoteiro..."

--- src/controllers/opera.deposito.ctrl.ts
+++ src/controllers/opera.deposito.ctrl.ts
@@ -1,48 +1,52 @@
-import { Request, Response, json } from 'express'
+import { Request, Response } from 'express'
 import OpDepositoServ from '../services/opera.deposito.serv'
 
 class OpDepositoCtrl {
 
     // Função de acesso ao serviço de busca de operações de depósito.
-    public async getAll (req: Request, res: Response): Promise<Response> {
-        const depositos = await OpDepositoServ.getAll()
-        if(depositos.length > 0){
-            return res.status(200).json(depositos)
-        }else{
-            return res.status(500).send({
-                message: 'Nenhuma operação de depósito foi encontrada.'
-            });
-        } 
+    public async getAll(req: Request, res: Response): Promise<void> {
+        await OpDepositoServ.getAll()
+        .then((resposta) =>{
+            if (resposta.length > 0) {
+                return res.status(200).json(resposta)
+            } else {
+                return res.status(500).send({
+                    message: 'Nenhuma operação de depósito foi encontrada.'
+                });
+            }
+        }).catch((error)=>{
+            return `Erro: ${error}`
+        })
+
+     
     }
 
     // Função de acesso ao serviço de cadastro de operações de depósitos.
-    public async create (req: Request, res: Response): Promise<Response> {
-        const comprovanteDeposito = await OpDepositoServ.create(req.body)
-
-        if(comprovanteDeposito == 2){
-            return  res.status(401).send({
-                message: 'Não foi possível realizar o depósito: Dados Inválidos.'
-            });
-        }
-
-        if(comprovanteDeposito == true){
-            return res.status(200).send({
-                message: 'Depósito realizado com sucesso.'
+    public async create(req: Request, res: Response): Promise<void> {
+        await OpDepositoServ.create(req.body)
+            .then((resposta) => {
+                switch (resposta) {
+                    case 2:
+                        return res.status(401).send({
+                            message: 'Não foi possível realizar o depósito: Dados Inválidos.'
+                        });
+                    case true:
+                        return res.status(200).send({
+                            message: 'Depósito realizado com sucesso.'
+                        })
+                    case false:
+                        return res.status(500).send({
+                            message: 'Falha ao emitir comprovante de depósito.'
+                        });
+                    default:
+                        return res.status(500).send({
+                            message: 'Falha ao realizar depósito.'
+                        });
+                }
+            }).catch((error) => {
+                return `Erro: ${error}`
             })
-            
 
-        }else if(comprovanteDeposito == false){
-            return res.status(500).send({
-                message: 'Falha ao emitir comprovante de depósito.'
-            });
-        }
-
-        if(comprovanteDeposito == undefined) {
-            return res.status(500).send({
-                message: 'Falha ao realizar depósito.'
-            });
-        }
     }
 }
-
 export default new OpDepositoCtrl();
