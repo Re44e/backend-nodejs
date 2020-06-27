bda87c9bf6f8aa2b484cee61d27fb7ce02c21d95
Re44e <re44e@outlook.com.br>
2020-06-26 23:19:22 -0300

    "Regra do escoteiro..."

--- src/controllers/saque.ctrl.ts
+++ src/controllers/saque.ctrl.ts
@@ -4,30 +4,39 @@ import ServSaques from '../services/saque.serv'
 class SaqueCtrl {
     
     // Função de acesso ao serviço de busca de configurações de saque.
-    public async getAll (req: Request, res: Response): Promise<Response> {
-        const saques = await ServSaques.getAll()
-        if(saques.length > 0){
-            return res.status(200).json(saques)
-        }else {
-            return res.status(500).send({
-                message: 'Nenhuma configuração de saque foi encontrada.'
-            });
-        }
+    public async getAll (req: Request, res: Response): Promise<void> {
+        await ServSaques.getAll()
+        .then((reposta)=>{
+            if(reposta.length > 0){
+                return res.status(200).json(reposta)
+            }else {
+                return res.status(500).send({
+                    message: 'Erro ao buscar configurações de saque.'
+                });
+            }
+        }).catch((error) => {
+            return `Erro: ${error}`
+        })
     }
 
     // Função de acesso ao serviço de cadastro de configurações de saque.
-    public async create (req: Request, res: Response): Promise<Response> {
-        const saque = await ServSaques.create(req.body)
+    public async create (req: Request, res: Response): Promise<void> {
+        await ServSaques.create(req.body)
+        .then((resposta)=>{
+            if(resposta){
+                return res.status(200).send({
+                    message: 'Configurações de saque cadastradas com sucesso.'
+                })
+            }else{
+                return res.status(500).send({
+                    message: 'Erro ao cadastrar configurações de saque.'
+                });
+            }
+        }).catch((error) => {
+            return `Erro: ${error}`
+        })
 
-        if(saque){
-            return res.status(200).send({
-                message: 'Configurações de saque cadastradas com sucesso.'
-            })
-        }else{
-            return res.status(500).send({
-                message: 'Erro ao cadastrar configurações de saque.'
-            });
-        }
+
     }
 }
 
